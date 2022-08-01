2d69cc901e9755d540f0da8b08e17d13f7a11750
jtylorlane <jtylorlane@yahoo.com>
2022-08-01 16:50:51 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      arrayOfBeer: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://api.punkapi.com/v2/beers")
+      .then((response) => {
+        return response.json();
+      })
+      .then((arrayOfBeer) => this.setState({ arrayOfBeer }));
+  }
+
+  handleClick = () => {
+    alert("You Like That! (Kirk Cousins voice)")
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>   Your component works</h2>
+        <ol>
+          {this.state.arrayOfBeer.map((beer, index) => {
+            return (
+              <li key={index}>
+                <h3>{beer.name}</h3>
+                <strong> Attuention Level: </strong>
+                <span>{beer.attenuation_level}</span><hr></hr>
+                <strong> Boil Volume by Unit: </strong>
+                <span>{beer.boil_volume.unit}</span><hr></hr>
+                <strong> Boil Volume by Value: </strong>
+                <span>{beer.boil_volume.value}</span><hr></hr>
+                <strong> Brewer Tips: </strong>
+                <span>{beer.brewers_tips}</span><hr></hr>
+                <strong> Contributed by: </strong>
+                <span>{beer.contributed_by}</span><hr></hr>
+                <strong> Description: </strong>
+                <span>{beer.description}</span><hr></hr>
+                <strong> EBC: </strong>
+                <span>{beer.ebc}</span><hr></hr>
+                <button onClick={this.handleClick}> I like it lot</button> 
+              </li>
+              
+            );
+          })}
+        </ol>
+      </div>
+    );
+  }
 }
 
 export default App;
